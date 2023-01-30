cbbff883f358a54d48d18225afe926e86cc2094d
TIEN BUI BA <buibatien2408@gmail.com>
2023-01-30 09:03:37 +0700

    add api properties swagger

--- shoe-server/src/comment-products/dto/create-comment-product.dto.ts
+++ shoe-server/src/comment-products/dto/create-comment-product.dto.ts
@@ -1,12 +1,15 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { IsNumber, IsOptional, IsString, Max, Min } from 'class-validator';
 
 export class CreateCommentProductDto {
   @IsString()
   @IsOptional()
+  @ApiPropertyOptional({ type: String })
   content?: string;
 
   @IsNumber()
   @Min(1)
   @Max(5)
+  @ApiProperty({ type: Number })
   ratingStar: number;
 }
--- shoe-server/src/orders/dto/order-create-dto.ts
+++ shoe-server/src/orders/dto/order-create-dto.ts
@@ -1,9 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsArray, IsNumber } from 'class-validator';
 
 export class CreateOrderDto {
   @IsArray()
+  @ApiProperty({ type: Array<number> })
   cartIds: Array<number>;
 
   @IsNumber()
+  @ApiProperty({ type: Number })
   addressId: number;
 }
