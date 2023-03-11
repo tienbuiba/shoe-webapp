40d4f7befcb2b04a7017620d98733244ad12a211
tienbuiba <buibatien2408@gmail.com>
2023-03-11 15:53:36 +0700

    add icon call

--- shoe-customer/src/pages/HomePage.js
+++ shoe-customer/src/pages/HomePage.js
@@ -171,6 +171,22 @@ const HomePage = () => {
             <MainNews />
           </div>
         </div>
+        <div class="hotline-phone-ring-wrap">
+          <div class="hotline-phone-ring">
+            <div class="hotline-phone-ring-circle"></div>
+            <div class="hotline-phone-ring-circle-fill"></div>
+            <div class="hotline-phone-ring-img-circle">
+              <a href="tel:0986909682" class="pps-btn-img">
+                <img src={require("../_mock/icon-call.png")} alt="Gọi điện thoại" width="50" />
+              </a>
+            </div>
+          </div>
+          <div class="hotline-bar">
+            <a href="tel:0986909682">
+              <span class="text-hotline">0986.909.682</span>
+            </a>
+          </div>
+        </div>
         <Newsletter />
       </div>
       <Footer />
